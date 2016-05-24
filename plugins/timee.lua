ccee9a0c16eaf7e852796f995c9f6f77c3e01a95
mobinlord <mobinlord29@gmail.com>
2016-05-24 14:21:29 +0430

    Rename time.lua to timee.lua

---
+++ plugins/timee.lua
@@ -0,0 +1,22 @@
+--[[
+#
+#    Time And Date
+#
+#    @redteam_01_32
+#	@shadowking_ch
+#
+#
+]]
+
+function run(msg, matches)
+local url , res = http.request('http://api.gpmod.ir/time/')
+if res ~= 200 then return "No connection" end
+local jdat = json:decode(url)
+local text = '馃晵 爻丕毓鬲 '..jdat.FAtime..' \n馃搯 丕賲乇賵夭 '..jdat.FAdate..' 賲蹖亘丕卮丿.\n    ----\n馃晵 '..jdat.ENtime..'\n馃搯 '..jdat.ENdate.. '\n@shadowking_TG'
+return text
+end
+return {
+  patterns = {"^[/!#]([Tt][iI][Mm][Ee])$"}, 
+run = run 
+}
+
